05fea4f692e43c1474ddabfc0363f2193b908f5b
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2024-09-07 00:24:16 -0300

    :wrench: chore(learning-lab): Criando padronização de commits

---
+++ cz.js
@@ -0,0 +1,78 @@
+module.exports = {
+  prompter(cz, commit) {
+    console.log('\nPersonalize sua mensagem de commit!\n');
+    cz.prompt([
+      {
+        type: 'list',
+        name: 'type',
+        message: 'Selecione o tipo de alteração que você está fazendo:',
+        choices: [
+            {
+                name: 'WIP: Trabalho em progresso',
+                value: ':construction: WIP'
+            },
+            {
+                name: 'feat: Uma nova funcionalidade',
+                value: ':sparkles: feat'
+            },
+            { 
+                name: 'fix: Correção de um bug',
+                value: ':bug: fix'
+            },
+            { 
+                name: 'style: Mudanças no estilo (formatação do código, etc)',
+                value: ':ok_hand: style'
+            },
+            {
+                name: 'refactor: Mudanças no código que não corrigem bug ou adicionam feature',
+                value: ':recycle: refactor'
+            },
+            {
+                name: 'test: Adicionando ou corrigindo testes',
+                value: ':test_tube: test'
+            },
+            {
+                name: 'chore: Atualizações em tarefas de build ou configurações',
+                value: ':wrench: chore'
+            },
+        ]
+    },
+    {
+        type: 'list',
+        name: 'scope',
+        message: 'Qual escopo da alteração:',
+        choices: [
+            {
+                name: 'vue-quasar: Alterações no projeto vue-quasar',
+                value: 'vue-quasar'
+            },
+            {
+                name: 'database: Alterações no projeto database',
+                value: 'database'
+            },
+            {
+                name: 'learning-lab: Alterações no projeto raiz (learning-lab)',
+                value: 'learning-lab'
+            }
+        ]  
+    },
+    {
+        type: 'input',
+        name: 'subject',
+        message: 'Escreva uma mensagem curta e objetiva:\n'
+    },
+    {
+        type: 'input',
+        name: 'body',
+        message: 'Forneça uma descrição mais detalhada da mudança (opcional):\n'
+    },
+    
+    ]).then((answers) => {
+        let commitMessage = `${answers.type}`;
+        commitMessage += `${answers.scope ? `(${answers.scope})` : ''}`;
+        commitMessage += `: ${ answers.subject } \n\n${ answers.body }`;
+        
+        commit(commitMessage);
+    });
+  }
+};
