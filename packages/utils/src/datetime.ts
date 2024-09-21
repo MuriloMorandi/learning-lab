344a51d4502cfeec5da2b9c2ef93e994f3c76b56
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2024-09-21 18:50:38 -0300

    :test_tube: test: criado teste do pacote de uteis

--- packages/utils/src/datetime.ts
+++ packages/utils/src/datetime.ts
@@ -1,4 +1,6 @@
 import { format, parseISO } from 'date-fns';
+import { tz } from "@date-fns/tz";
+
 
 export function parseDate(date: string): Date {
   return parseISO(date);
@@ -15,13 +17,24 @@ export function formatDateTime(date: Date | string): string {
   if (typeof date === 'string') {
         date = parseDate(date);
     }
-    return format(date, 'dd/MM/yyyy HH:mm');
+  return format(date, 'dd/MM/yyyy HH:mm', {
+    in: tz('America/Sao_Paulo')
+  });
 }
 
 export function formatTime(date: Date|string): string {
   if (typeof date === 'string') {
         date = parseDate(date);
     }
-    return format(date, 'HH:mm');
+  
+  return format(date, 'HH:mm', {
+    in: tz('America/Sao_Paulo')
+  });
 }
 
+export function formatDayMonthLong(date: Date) {
+  return new Intl.DateTimeFormat('pt-BR', {
+    day: '2-digit',
+    month: 'long',
+  }).format(date);
+}
